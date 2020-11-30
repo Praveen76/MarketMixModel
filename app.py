40744bd901d23910559c5a53d53a90bf0877cfe2
Praveen Kumar Anwla <praveenkumar.kumar76@gmail.com>
2020-11-30 15:15:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import datetime as dt
 from scipy.special import boxcox, inv_boxcox
 
 from flask import Flask, request, jsonify, render_template, url_for
+from sklearn.preprocessing import PolynomialFeatures
+
 #from model import encode
 import pickle
 import operator
@@ -22,29 +24,28 @@ print('Done1')
 
 model = pickle.load(open('./model.sav','rb'))
 
-fitted_lambda=0.09785996954548055
+polynomial_features= PolynomialFeatures(degree=2)
+fitted_lambda=-0.3459788735069217
 
-meanDict={'Internet_LCount': 68.61194029850746,
- 'TV_LCount': 45.13636363636363,
- 'Radio_LCount': 38.803030303030305,
- 'Other_LCount': 41.19402985074627,
- 'Internet_Exp': 170618.64104477613,
- 'TV_Exp': 158890.0026865672,
- 'Radio_Exp': 101947.55268656714,
- 'Others_Exp': 23182.348208955224,
- 'FL_Amt': 33353612.334995154,
- 'Range': 33.0}
+meanDict={'Internet_LCount': 2339.616666666667,
+ 'Radio_LCount': 325.21666666666664,
+ 'TV_LCount': 543.85,
+ 'Other_LCount': 460.26666666666665,
+ 'Internet_Exp': 206289.14133333325,
+ 'Radio_Exp': 88113.31699999998,
+ 'TV_Exp': 144973.2666666667,
+ 'Other_Exp': 19821.117833333337,
+ 'FL_Amt': 24341066233.662113}
 
-stdDict={'Internet_LCount': 65.47957239264514,
- 'TV_LCount': 14.411576116816608,
- 'Radio_LCount': 12.097106712650906,
- 'Other_LCount': 27.820440929543995,
- 'Internet_Exp': 122307.46732575353,
- 'TV_Exp': 73365.93650074594,
- 'Radio_Exp': 41057.893726632305,
- 'Others_Exp': 9281.540160142722,
- 'FL_Amt': 26433128.11441197,
- 'Range': 19.485036994233976}
+stdDict={'Internet_LCount': 779.7722634002581,
+ 'Radio_LCount': 87.88669355456946,
+ 'TV_LCount': 181.3443045139853,
+ 'Other_LCount': 450.9599503682763,
+ 'Internet_Exp': 78335.36024980673,
+ 'Radio_Exp': 21768.13628693292,
+ 'TV_Exp': 54230.19755724163,
+ 'Other_Exp': 10596.293305193014,
+ 'FL_Amt': 105480068914.12782}
 
 def Transformation(demoDf):
     for col in demoDf:
@@ -70,19 +71,21 @@ def predict():
     print('int_features',int_features)
     test=pd.DataFrame.from_records([{
                     'Internet_LCount':int_features[0],
-                    'TV_LCount':int_features[1],
-                      'Radio_LCount':int_features[2],
+                    'Radio_LCount':int_features[1],
+                      'TV_LCount':int_features[2],
                        'Other_LCount':int_features[3],
                         'Internet_Exp':int_features[4],
-                        'TV_Exp'  :int_features[5],
-                        'Radio_Exp':int_features[6],
-                        'Others_Exp':int_features[7] }])
+                        'Radio_Exp'  :int_features[5],
+                        'TV_Exp':int_features[6],
+                        'Other_Exp':int_features[7] }])
     test=Transformation(test)
-    prediction = model.predict(test)
+    xp = polynomial_features.fit_transform(test)
+
+    prediction = model.predict(xp)
     predTrans=inv_boxcox(prediction,fitted_lambda)
     print(predTrans)
     predTrans=np.round(predTrans,2)
-    return render_template('home.html', prediction_text="Predicted funded loan amount : {}".format((predTrans)))
+    return render_template('home.html', prediction_text="Predicted funded loan amount : $ {}".format((predTrans)))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
