ebd4429b3220cf5806e7aed41faf3f576a88e69f
Praveen Kumar Anwla <praveenkumar.kumar76@gmail.com>
2020-12-09 10:53:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,52 +12,20 @@ import datetime as dt
 from scipy.special import boxcox, inv_boxcox
 
 from flask import Flask, request, jsonify, render_template, url_for
-from sklearn.preprocessing import PolynomialFeatures
 
 #from model import encode
 import pickle
 import operator
 
-app = Flask(__name__)
 
-print('Done1')
+app = Flask(__name__)
 
 model = pickle.load(open('./model.sav','rb'))
 
-polynomial_features= PolynomialFeatures(degree=2)
-fitted_lambda=-0.3459788735069217
+print('Params :',model.get_params())
+print('Feat  Imp :',model.feature_importances_)
+fitted_lambda= 0.03608400474708946
 
-meanDict={'Internet_LCount': 2339.616666666667,
- 'Radio_LCount': 325.21666666666664,
- 'TV_LCount': 543.85,
- 'Other_LCount': 460.26666666666665,
- 'Internet_Exp': 206289.14133333325,
- 'Radio_Exp': 88113.31699999998,
- 'TV_Exp': 144973.2666666667,
- 'Other_Exp': 19821.117833333337,
- 'FL_Amt': 24341066233.662113}
-
-stdDict={'Internet_LCount': 779.7722634002581,
- 'Radio_LCount': 87.88669355456946,
- 'TV_LCount': 181.3443045139853,
- 'Other_LCount': 450.9599503682763,
- 'Internet_Exp': 78335.36024980673,
- 'Radio_Exp': 21768.13628693292,
- 'TV_Exp': 54230.19755724163,
- 'Other_Exp': 10596.293305193014,
- 'FL_Amt': 105480068914.12782}
-
-def Transformation(demoDf):
-    for col in demoDf:
-        for i, row_value in demoDf[col].iteritems():
-            mean=meanDict[col]
-            std=stdDict[col]
-            newVal=(row_value-mean)/std
-            demoDf[col][i] = newVal
-    return demoDf
-
-
-print('Done2')
     
 @app.route('/')
 def home():
@@ -71,19 +39,16 @@ def predict():
     print('int_features',int_features)
     test=pd.DataFrame.from_records([{
                     'Internet_LCount':int_features[0],
-                    'Radio_LCount':int_features[1],
-                      'TV_LCount':int_features[2],
+                    'TV_LCount':int_features[1],
+                      'Radio_LCount':int_features[2],
                        'Other_LCount':int_features[3],
                         'Internet_Exp':int_features[4],
-                        'Radio_Exp'  :int_features[5],
-                        'TV_Exp':int_features[6],
+                        'TV_Exp'  :int_features[5],
+                        'Radio_Exp':int_features[6],
                         'Other_Exp':int_features[7] }])
-    test=Transformation(test)
-    xp = polynomial_features.fit_transform(test)
-
-    prediction = model.predict(xp)
+    print('CWD :',os.getcwd())
+    prediction = model.predict(test)
     predTrans=inv_boxcox(prediction,fitted_lambda)
-    print(predTrans)
     predTrans=np.round(predTrans,2)
     return render_template('home.html', prediction_text="Predicted funded loan amount : $ {}".format((predTrans)))
 
